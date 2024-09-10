48ebef4144156571d108c21ca72331eebfbfcae4
JdDonovanSb <donovanboni@gmail.com>
2024-09-10 12:59:13 -0500

    Add the files certificate.pem. csr.pem, privatekey.pem to correct funtionally ssl and add the corrections to index for a correct funtionally, and delete the file hello-world.js

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-const usuarios = require('./controllers/usuarios');
-const cursos = require('./controllers/cursos');
 const express = require('express');
+const https = require ('https');
+const fs = require ('fs');
+const path = require('path');
 const mongoose = require('mongoose'); 
+const cors = require ('cors'); //importa el midelware de cors
+const { swaggerUi, swaggerSpec } = require('./swagger/Swagger'); // Importa Swagger
+
 // Conexión a la dase de datos mongodb
-mongoose.connect ('mongodb+srv://donovanboni:AMGhSKt7mD0eApIf@cluster0.3kkiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Conectado a MongoDB.. '))
-    .catch(err => console.log('No se pudo conectar con MongoDB..', err));
-// middlewareconst 
-app = express();
+
+// Importar rutas
+const cursosRoutes = require('./routes/cursos_routes');
+const usuariosRoutes = require('./routes/usuarios_routes');
+
+// Importar la función de semillas
+const seedDatabase = require('./seed/seeds');
+
+//midelware
+// Crear la aplicación Express
+const app = express();
+
+const corsOptions = {
+    origin: '*', //reemplaza con el dominio permitido 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], //METODOS PERMITIDOS
+    allowedHeaders: ['Content-Type', 'Authorization'], //encabezados permitidos
+};
+app.use(cors(corsOptions));//Habilita cors con las acciones especificas
+app.options('*', cors(corsOptions));//
+
+//Carga el certificado SSl y la Clave privada
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'privatekey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'certificate.pem')),
+};
+
 app.use (express.json());
 app.use (express.urlencoded ({extended: true}));
+// Configuración de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-//end-points  (recursos)
+// Conexión a la dase de datos mongodb
+mongoose.connect ('mongodb+srv://donovanboni:AMGhSKt7mD0eApIf@cluster0.3kkiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(async () => {
+    console.log('Conectado a MongoDB');
 
+        // Ejecutar la siembra de la base de datos
+        await seedDatabase();
+
+/*Endpoint recurso rutas
 app.use('/api/usuarios', usuarios);
 app.use('/api/cursos', cursos);
+*/
 
 
-const port = process.env.PORT || 3000;
-app. listen(port, () => {
-    console. log('Api REST Ok, y ejecutándose...');
+// Rutas de la aplicación
+app.use('/api/cursos', cursosRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+
+ // Manejo de errores
+ app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+
+ // Iniciar el servidor
+ const PORT = process.env.PORT || 3000;
+
+ https.createServer(options, app).listen(PORT, () => {
+    console.log('Api Rest OK y ejecutandose...');
+    console.log('Servidor https corriendo en https://localhost:3000');
+});
+
 })
+.catch(err => console.log('No se pudo conectar con MongoDB..', err));
